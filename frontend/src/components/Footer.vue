8e191cbbba7f3bcfaa548ca1f09b94eb5da65579
manav1403 <manavavni@gmail.com>
2020-08-19 20:23:03 +0530

    segmented the home page and added about us

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  div.wrapper(
+    :style= "{ '--hero-image': `url(${HeroImg}) no-repeat` }"
+  )
+    div.container
+</template>
+<script>
+import HeroImg from '@/assets/footer-bg1.png'
+export default {
+  name: 'Footer',
+  data: () => ({
+    HeroImg
+  })
+}
+</script>
+<style scoped>
+  .wrapper{
+    background: var(--hero-image);
+    background-size: cover;
+    padding: 80px 0;
+    margin-top: 100px;
+    margin: 0;
+    box-sizing: border-box;
+  }
+</style>
---
+++ frontend/src/components/home/Banner2.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  div.wrapper
+      div(class="wrapper-background"
+          :style= "{ '--hero-image': `url(${HeroImg})` }"
+        )
+      div.wrapper-content
+</template>
+<script>
+import HeroImg from '@/assets/bg-1.jpg'
+export default {
+  name: 'Banner2',
+  components: {
+  },
+  data: () => ({
+    HeroImg
+  })
+}
+</script>
+<style scoped>
+  .h-100{
+    height: 100%;
+    overflow: visible;
+  }
+  .wrapper {
+    overflow: visible;
+    position: relative;
+    background: color 1c 1a 17;
+    padding: 0;
+    /* -webkit-transition-property: height;
+    -webkit-transition-duration: 0.3s; */
+    transition-timing-function: ease-in;
+    box-sizing: border-box;
+    width: 100vw !important;
+    height: 100vh !important;
+    z-index: 1;
+  }
+  .wrapper-background{
+    width: 101%;
+    height: 100%;
+    background-image: var(--hero-image);
+    box-sizing: border-box;
+    background-size: cover;
+    background-position: center center;
+  }
+  .wrapper-content{
+    background: rgb(33,0,67,0.9);
+    width: 100%;
+    height: 100%;
+    position: absolute !important;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    overflow: visible;
+  }
+</style>
