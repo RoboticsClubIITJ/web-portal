8efec22b922d3014cf22d2f1a8d2b84db268b4ac
DarshitJain04 <jain.38@iitj.ac.in>
2020-08-22 23:37:36 +0530

    made baseURL dynamic

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,11 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api/*': {
-        target: `http://${process.env.BACKEND_HOST !== '' ? process.env.BACKEND_HOST : '127.0.0.1'}:8000`,
-        secure: false
-      },
-      '/auth/*': {
-        target: `http://${process.env.BACKEND_HOST !== '' ? process.env.BACKEND_HOST : '127.0.0.1'}:8000`,
+        target: `http://${process.env.BACKEND_HOST ? process.env.BACKEND_HOST : '127.0.0.1'}:8000`,
         secure: false
       }
     }
