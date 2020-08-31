6ff2cbe548942dc9bb37be119f5850f72c162c59
Kodavali841 <56919318+Kodavali841@users.noreply.github.com>
2020-08-31 09:37:29 +0530

    404 comingsoon (#23)
    * added 404 page, coming soon page
    
    * added routes to the appbar , updated router
    
    Co-authored-by: manav1403 <manavavni@gmail.com>
    Co-authored-by: Manav Kapoor <51405571+manav1403@users.noreply.github.com>

---
+++ frontend/src/views/Comingsoon.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+      Launchingsoon
+</template>
+<script>
+import Launchingsoon from '@/components/Launchingsoon'
+export default {
+  name: 'Comingsoon',
+
+  components: {
+    Launchingsoon
+  },
+
+  data: () => ({
+    drawer: true
+  })
+}
+</script>
+<style>
+  .main-wrapper{
+    padding:0;
+  }
+  .banners{
+    overflow: hidden;
+  }
+</style>
