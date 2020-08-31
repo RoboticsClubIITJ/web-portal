af803958cbe4b0daea787f89bc040a1f2945aaf2
manav1403 <manavavni@gmail.com>
2020-08-31 11:24:16 +0530

    added model and api  for profile cards in homepage

--- backend/team/urls.py
+++ backend/team/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+    path('homeview', views.HomeTeamList.as_view(), name="team-homeview"),
     path('', views.MembersList.as_view(), name="team-members"),
 ]
