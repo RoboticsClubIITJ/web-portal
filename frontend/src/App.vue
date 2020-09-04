6da3198f45a064e68402bd9f618106b920c55762
Manav Kapoor <51405571+manav1403@users.noreply.github.com>
2020-09-04 22:02:50 +0530

    Added Profile create page and corresponding API back-end and corrected login routes workflow to login logout of studentzone    (#29)
    * initiated the create profile page
    
    * added api for post to create profile
    
    * added csrf verification
    
    * end card layout
    
    * corrected merge error
    
    * added logout section

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
   v-app
-    div.main-content-wrapper
-      AppBar(v-if="!$route.meta.NoAP")
+    div(:class="!!$route.meta.Background ? 'main-content-wrapper' : ''")
+      AppBar(v-if="!!$route.meta.AppBar")
       router-view
-      Footer(v-if="!$route.meta.NoAP")
+      Footer(v-if="!!$route.meta.Footer")
 </template>
 
 <script>
 import AppBar from '@/components/AppBar'
 import Footer from '@/components/Footer'
+import { instance } from '@/api/axios'
 export default {
   name: 'App',
 
@@ -18,10 +19,17 @@ export default {
   },
 
   data: () => ({
-  })
+  }),
+
+  created () {
+    instance.get('/auth/csrf-token/').then(response => {
+      const token = response.data.csrftoken
+      document.cookie = `csrftoken = ${token}`
+    })
+  }
 }
 </script>
-<style>
+<style scoped>
   .main-content-wrapper{
     padding:0;
     background: linear-gradient(180deg,#240044 0,#0f0240 25%,#400959 40%,#0f0240 65%,#0f0240);
