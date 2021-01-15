4171e2717b625ca66941038e536fa3de2ff9747f
Manav Kapoor <51405571+manav1403@users.noreply.github.com>
2021-01-15 19:31:43 +0530

    Update Projects.vue

--- frontend/src/views/Projects.vue
+++ frontend/src/views/Projects.vue
@@ -11,7 +11,7 @@
     class="mx-auto cardstyle"
   >
     <v-img
-      :src="project.img_tile"
+      :src="project.img_tile.pathname"
       height="200px"
       gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
       style="margin-bottom:10px;"
