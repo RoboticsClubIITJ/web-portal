06adc13908a51683310c357949c696f5d4dc361a
Naman Goyal <naman280104@gmail.com>
2023-02-24 00:54:36 +0530

    trying url

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,15 +2,15 @@ module.exports = {
   devServer: {
     proxy: {
       '/api/*': {
-        target: `http://${process.env.BACKEND_HOST ? process.env.BACKEND_HOST : '127.0.0.1'}:8000`,
+        target: `https://${process.env.BACKEND_HOST ? process.env.BACKEND_HOST : '63f7b30c2db7870008628742--helpful-figolla-146d54.netlify.app/'}`,
         secure: false
       },
       '/django_media/*': {
-        target: `http://${process.env.BACKEND_HOST ? process.env.BACKEND_HOST : '127.0.0.1'}:8000`,
+        target: `https://${process.env.BACKEND_HOST ? process.env.BACKEND_HOST : '63f7b30c2db7870008628742--helpful-figolla-146d54.netlify.app/'}`,
         secure: false
       },
       '/django_static/*': {
-        target: `http://${process.env.BACKEND_HOST ? process.env.BACKEND_HOST : '127.0.0.1'}:8000`,
+        target: `https://${process.env.BACKEND_HOST ? process.env.BACKEND_HOST : '63f7b30c2db7870008628742--helpful-figolla-146d54.netlify.app/'}`,
         secure: false
       }
     }
