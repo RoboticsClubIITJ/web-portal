af6fe6b712bf9c0c6806dde3e2517b4d17456db3
Naman Goyal <naman280104@gmail.com>
2023-02-24 01:58:48 +0530

    baseurl changed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'localhost:8080/api',
+  baseURL: 'localhost:8000/api',
   xsrfCookieName: 'csrftoken',
   xsrfHeaderName: 'X-CSRFToken'
 })
