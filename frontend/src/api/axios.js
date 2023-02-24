4f80ca40141f2f4e05a4317f86b7aacc52e947ea
Naman Goyal <naman280104@gmail.com>
2023-02-25 00:08:28 +0530

    minor changes in production docker file, base url changes for /api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: '/api',
   xsrfCookieName: 'csrftoken',
   xsrfHeaderName: 'X-CSRFToken'
 })
